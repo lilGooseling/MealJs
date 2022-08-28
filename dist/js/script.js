006af592f2e022e6ae771f10bfb0af60a9ae683c
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-08-28 22:17:16 +0300

    Tabs

--- dist/js/script.js
+++ dist/js/script.js
@@ -100,4 +100,46 @@
 /******/ });
 //# sourceMappingURL=script.js.map
 
+window.addEventListener('DOMContentLoaded', () => {
 
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            //item = tab
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent(); //i=0 - первый слайд по умолчанию подставляется, если аргумент при вызове функции не задан
+
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+
+    })
+
+
+})

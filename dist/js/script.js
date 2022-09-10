fb575c40e713969472d3de1570c0946838a36771
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-09-10 22:17:33 +0300

    Создание таймера обратного отсчета на сайте,2

--- dist/js/script.js
+++ dist/js/script.js
@@ -167,9 +167,38 @@ window.addEventListener('DOMContentLoaded', () => {
         };
     }
 
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
 
+    function setClock(selector, endtime) {
+        const timer =  document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
 
+        updateClock();    //что бы таймер на странице подгружался сразу же
 
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
 
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
 
-})
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+});

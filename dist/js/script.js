5e83034b1e28eea16bd878da9b51dbb35cbc1119
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-09-13 21:34:48 +0300

    Создание модального окна,2

--- dist/js/script.js
+++ dist/js/script.js
@@ -229,11 +229,26 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
-    modalCloseBtn.addEventListener('click', () => {
+    function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove('show');
         //modal.classList.toggle('show');
         document.body.style.overflow = '';
+    }
+
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
     });
 
 

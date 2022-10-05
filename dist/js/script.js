e81d287e65056fa59743de7c509317b2b56ad367
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-10-05 22:28:38 +0300

    JSON-server

--- dist/js/script.js
+++ dist/js/script.js
@@ -482,7 +482,9 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
-
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
 
 
 

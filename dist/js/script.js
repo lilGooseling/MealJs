403d2eb5375acbd8443f9023081efce4bae4ba5e
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-10-11 11:48:50 +0300

    Регулярные выражения при работе со слайдом

--- dist/js/script.js
+++ dist/js/script.js
@@ -523,13 +523,16 @@ window.addEventListener('DOMContentLoaded', () => {
         dots.push(dot);
     }
 
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
 
 
     next.addEventListener('click', () => {
-        if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)){
+        if (offset === deleteNotDigits(width) * (slides.length - 1)){
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -553,9 +556,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset === 0){
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -582,7 +585,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
             slideIndex = slideTo;
 
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
